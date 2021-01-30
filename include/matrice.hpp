9db064bd5a3a13405845fc4a2bb2b2a8265be592
Mouzhi Othmane <othmane.mouzhi@ensta-paris.fr>
2021-01-30 11:25:08 +0100

    changement2

--- include/matrice.hpp
+++ include/matrice.hpp
@@ -1,54 +1,72 @@
-class matrice
+#ifndef __MATRICE__
+#define __MATRICE__
+#include <string>
+
+using namespace std;
+template<typename T>
+class Vecteur;
+class Matrice
 {
-protected:
-    unsigned int dim_l, dim_c, size_; // dimension de la matrice, taille du vecteur val_
-    double *val_;                     // tableau des valeurs de la matrice
+  public:
+	int n, m;	// Nombre de lignes, colonnes
+	double **val; // Tableau de valeurs
 
-public:
-    virtual ~matrice();
-    unsigned int dim() const { return size_; }; // retourne la taille du vecteur val_
+	// Constructeurs
+	Matrice();								 // Constructeur par défaut
+	Matrice(int n_l, int n_c);				 // Constructeur avec dimension, initialisation à 0
+	Matrice(int n_l, int n_c, double valeur); // Constructeur avec dimension + une valeur
+	Matrice(const Matrice &M);				 // Constructeur par copie
+	Matrice(int n, const string &id);		 // Contructeur matrice identité
+	// Destructeur
+	~Matrice();
 
-    void alloc(unsigned int d)
-    {
-        size_ = d;
-        val_ = new double[size_];
-    };
-    void dealloc()
-    {
-        if (val_ != nullptr)
-        {
-            delete[] val_;
-        }
-    };
+	// Opérateurs
 
-    virtual double &operator()(unsigned int i, unsigned int j) = 0;
+	Matrice &operator=(const Matrice &M);
+	double &operator()(int i, int j) const;
 
-    matrice *copie_val(double);
-    matrice *copie_val(matrice &);
+	Matrice operator+(const Matrice &M) const;
+	Matrice operator-(const Matrice &M) const;
+	Matrice operator*(const Matrice &M) const;
+	Vecteur<double> operator*(const Vecteur<double> &V) const;
 
-    // matrice &operator+=(const matrice &);
-    // matrice &operator*=(const matrice &);
-    // matrice &operator/=(const double);
+	Matrice operator*(const double nb) const;
+	Matrice operator/(const double nb) const;
 
-    // virtual void print() = 0;
+	Matrice &operator/=(const double nb);
+	Matrice &operator*=(const double nb);
+
+	Matrice &operator+=(const Matrice &M);
+	Matrice &operator-=(const Matrice &M);
+
+	double norm();
+	// Opérateurs d'affichage
+
+	void print();
+	friend ostream &operator<<(ostream &, const Matrice &M);
 };
 
-class matrice_pleine : public matrice
-{
-public:
-    matrice_pleine(unsigned int d = 0, const double &v0 = 0); //dim et val constante
-    matrice_pleine(const matrice &);                          //constructeur par copie
+Matrice transpose(const Matrice &M);
+Vecteur<double> toVecteur(const Matrice &M);
 
-    double &operator()(int i, int j);
-    // void print();
+
+
+class Matrice_diag
+{ // classe pour les matrices carrées diagonales
+public:
+  int n; //taille de la matrice carrée
+  double *val; //tableau des valeurs (diagonale uniquement)
+
+  Matrice_diag();								 // Constructeur par défaut
+	Matrice_diag(int n_l, double valeur); // Constructeur avec dimension + une valeur
+
+  // Destructeur
+  ~Matrice_diag();
+
+  friend ostream &operator<<(ostream &, const Matrice_diag &M);
+  Matrice_diag &operator=(const Matrice_diag &M);
+  Matrice_diag operator*(const double nb) const;
+  Vecteur<double> operator*(const Vecteur<double> &V) const;
 };
 
-class matrice_bande : public matrice
-{
-public:
-    matrice_bande(unsigned int d = 0, const double &v0 = 0); //dim et val constante
-    matrice_bande(const matrice &);                          //constructeur par copie
-
-    double &operator()(int i, int j);
-    // void print();
-};
+#endif
--- include/vecteur.hpp
+++ include/vecteur.hpp
@@ -1,81 +1,267 @@
-#ifndef vecteur_h
-#define vecteur_h
-
+#ifndef __VECTEUR__
+#define __VECTEUR__
 #include <iostream>
+#include <cmath>
+#include "matrice.hpp"
 using namespace std;
 
-//utilitaires
-void stop(const char *msg);                     //message d'arr�t
-void test_dim(int d1, int d2, const char *org); //test dimension
-
-//classe vecteur de r�els double pr�cision
 template <typename T>
-class vecteur
+class Vecteur
 {
-private:
-    int dim_; //dimension du vecteur
-    T *val_;  //tableaux de valeurs
+  public:
+    int dim;
+    T *val;
+    //Constructeurs
+    Vecteur();
+    Vecteur(int d, T x = T(0));
+    Vecteur(const Vecteur &V);
 
-public:
-    vecteur(int d = 0, const T &v0 = 0); //dim et val constante
-    vecteur(const vecteur<T> &v);        //constructeur par copie
-    ~vecteur();
+    //Destructeur
+    ~Vecteur();
 
-    //tools
-    void init(int d);                //allocation
-    void clear();                    //d�sallocation
-    int dim() const { return dim_; } //acc�s dimension
+    //Opérateurs internes
+    Vecteur<T> &operator=(const Vecteur<T> &);
+    Vecteur<T> &operator*=(const T);
+    Vecteur<T> &operator/=(const T);
+    Vecteur<T> operator^(const Vecteur<T> &);
+    Vecteur<T> operator+(const Vecteur<T> &A);
+    Vecteur<T> operator-(const Vecteur<T> &A);
+    Vecteur<T> operator*(const T);
+    Vecteur<T> concatenate(const Vecteur<T> &A);
+    Matrice toMatrice(const int i, const int j) const;
+    T operator*(const Vecteur<T> &);
+    T &operator()(int) const;
+    Vecteur<T> operator()(int, int) const;
+    //Opérateur print
+    template <typename type>
+    friend ostream &operator<<(ostream &, const Vecteur<type> &);
+    //Norme
+    double norm();
+};
+//Opérateurs externes
+//Definition des methodes
 
-    //op�rateur d'assignation
-    vecteur<T> &operator=(const vecteur<T> &v); //assignation d'un vecteur
-    vecteur<T> &operator=(const T &x);          //assignation d'une valeur
-
-    //op�rateurs d'acc�s (pour les utilisateurs)
-    T &operator()(int i) const { return val_[i - 1]; } //valeur    1->dim
-    T &&operator()(int i) { return val_[i - 1]; }      //r�f�rence 1->dim
-
-    //op�rateurs alg�briques
-    vecteur<T> &operator+=(const vecteur<T> &v); // u += v
-    vecteur<T> &operator-=(const vecteur<T> &v); // u -= v
-    vecteur<T> &operator+=(const T &x);          // u += x
-    vecteur<T> &operator-=(const T &x);          // u -= x
-    vecteur<T> &operator*=(const T &x);          // u *= x
-    vecteur<T> &operator/=(const T &x);          // u /= x
-
-}; //fin de d�finition de la classe
-
-//op�rateurs externes
+//Constructeurs
 template <typename T>
-vecteur<T> operator+(const vecteur<T> &u); //+ unaire ne fait rien !
+Vecteur<T>::Vecteur() : dim(0), val(0){};
+
 template <typename T>
-vecteur<T> operator-(const vecteur<T> &u); //- unaire : chgt de signe
+Vecteur<T>::Vecteur(int d, T v)
+{
+    dim = d;
+    val = new T[dim];
+    for (int i = 0; i < dim; i++)
+    {
+        val[i] = v;
+    }
+};
 
-template <typename T, typename S>
-vecteur<T> operator+(const vecteur<T> &u, const vecteur<S> &v); // u + v
-template <typename T, typename S>
-vecteur<T> operator-(const vecteur<T> &u, const vecteur<S> &v); // u - v
-template <typename T, typename S>
-vecteur<T> operator+(const vecteur<T> &u, const S &x); // u + x
-template <typename T, typename S>
-vecteur<T> operator+(const T &x, const vecteur<S> &u); // x + u
-
-// vecteur operator-(const vecteur &u, double x);         // u - x
-// vecteur operator-(double x, const vecteur &u);         // x - u
-// vecteur operator*(const vecteur &u, double x);         // u * x
-// vecteur operator/(const vecteur &u, double x);         // u / x
-// vecteur operator+(double x, const vecteur &u);         // x + u
-// vecteur operator-(double x, const vecteur &u);         // x - u
-// vecteur operator*(double x, const vecteur &u);         // x * u
-// double operator|(const vecteur &u, const vecteur &v);  // u | v
-// double norme(const vecteur &u);                        // sqrt(u|u)
-
-// //op�rateurs de comparaison
-// bool operator==(const vecteur &u, const vecteur &v); // u == v
-// bool operator!=(const vecteur &u, const vecteur &v); // u != v
-
-//op�rateurs de lecture et d'�criture
 template <typename T>
-ostream &operator<<(ostream &os, const vecteur<T> &u); // os << u
+Vecteur<T>::Vecteur(const Vecteur<T> &V)
+{
+    dim = V.dim;
+    val = NULL;
+    if(dim<=0) return;
+    val = new T[dim];
+    for (int i = 0; i < dim; i++)
+    {
+        val[i] = V.val[i];
+    }
+};
 
-#include "vecteur.cpp"
+//Destructeur
+template <typename T>
+Vecteur<T>::~Vecteur()
+{
+    if (val != 0)
+        delete[] val;
+};
+
+//Opérations internes
+template <typename T>
+Vecteur<T> &Vecteur<T>::operator=(const Vecteur<T> &V)
+{
+    dim = V.dim;
+    if (val != 0)
+        delete[] val;
+    val = new T[dim];
+    for (int i = 0; i < dim; i++)
+    {
+        val[i] = V.val[i];
+    }
+    return *this;
+};
+
+template <typename T>
+Vecteur<T> &Vecteur<T>::operator*=(const T f)
+{
+    for (int i = 0; i < dim; i++)
+    {
+        val[i] *= f;
+    }
+    return *this;
+};
+
+template <typename T>
+Vecteur<T> &Vecteur<T>::operator/=(const T f)
+{
+    if (f == 0)
+    {
+        cout << "Erreur division par 0";
+        exit(1);
+    }
+    for (int i = 0; i < dim; i++)
+    {
+        val[i] /= f;
+    }
+    return *this;
+};
+
+template <typename T>
+T &Vecteur<T>::operator()(int i) const
+{
+    return val[i - 1];
+};
+
+template <typename T>
+Vecteur<T> Vecteur<T>::operator()(int i, int j) const
+{
+    if (i >= j)
+    {
+        cout << "i doit 6etre strictement inférieur à j \n";
+        exit(1);
+    }
+    int dim = j - i + 1;
+    Vecteur temp(dim);
+    for (int k = 0; k < dim; k++)
+    {
+        temp.val[k] = val[i + k];
+    }
+    return temp;
+}
+template <typename T>
+Vecteur<T> Vecteur<T>::operator+(const Vecteur<T> &A)
+{
+    if (A.dim != this->dim)
+    {
+        cout << "Les vecteurs n'ont pas la même dimension. \n";
+        exit(1);
+    }
+    Vecteur<T> C = *this;
+    for (int i = 0; i < A.dim; i++)
+    {
+        C.val[i] += A.val[i];
+    }
+    return C;
+};
+template <typename T>
+Vecteur<T> Vecteur<T>::operator-(const Vecteur<T> &A)
+{
+    if (A.dim != this->dim)
+    {
+        cout << "Les vecteurs n'ont pas la même dimension. \n";
+        exit(1);
+    }
+    Vecteur<T> C = *this;
+    for (int i = 0; i < A.dim; i++)
+    {
+        C.val[i] -= A.val[i];
+    }
+    return C;
+};
+
+template <typename T>
+Vecteur<T> Vecteur<T>::operator*(const T a)
+{
+    Vecteur V = *this;
+    for (int i = 0; i < dim; i++)
+    {
+        V.val[i] *= a;
+    }
+    return V;
+};
+
+template <typename T>
+Vecteur<T> Vecteur<T>::concatenate(const Vecteur<T> &A)
+{
+    Vecteur C(A.dim + this->dim);
+    for (int i = 0; i < this->dim; i++)
+    {
+        C.val[i] = this->val[i];
+    }
+    for (int j = 0; j < A.dim; j++)
+    {
+        C.val[j + this->dim] = A.val[j];
+    }
+    return C;
+};
+
+template <typename T>
+Matrice Vecteur<T>::toMatrice(const int i, const int j)const
+{
+    if (i * j != dim)
+    {
+        cout << "Erreur de dimension \n";
+        exit(1);
+    }
+    Matrice temp(i, j);
+    for (int k = 0; k < i; k++)
+    {
+        for (int l = 0; l < j; l++)
+        {
+            temp.val[k][l] = val[l+k*j];
+        }
+    }
+    return temp;
+}
+
+//Produit vectoriel en dimension 3
+template <typename T>
+Vecteur<T> Vecteur<T>::operator^(const Vecteur<T> &V)
+{
+    if ((dim != V.dim) || (dim > 3))
+    {
+        cout << "Les dimensions sont supérieures à 3 ou ne sont pas égales.\n";
+        exit(1);
+    }
+    Vecteur<T> result(3);
+
+    result(1) = val[1] * V(3) - val[2] * V(2);
+    result(2) = val[2] * V(1) - val[0] * V(3);
+    result(3) = val[0] * V(2) - val[1] * V(1);
+    return result;
+}
+
+//Produit scalaire
+template <typename T>
+T Vecteur<T>::operator*(const Vecteur<T> &V)
+{
+    T result = T(0);
+    for (int i = 0; i < dim; i++)
+    {
+        result += val[i] * V.val[i];
+    }
+    return result;
+}
+
+//Opérateur print
+template <typename T>
+ostream &operator<<(ostream &out, const Vecteur<T> &V)
+{
+    out << "( ";
+    for (int i = 0; i < V.dim; i++)
+    {
+        out << V.val[i] << " ";
+    }
+    out << ")\n";
+    return out;
+};
+
+//Norme
+template <typename T>
+double Vecteur<T>::norm()
+{
+    return sqrt(((*this) * (*this)));
+};
+
 #endif
