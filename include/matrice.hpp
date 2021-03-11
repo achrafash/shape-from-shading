0ee450303defcddc62e18326db27c02f6cd378b0
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-03-11 09:25:35 +0100

    Add init method to initialize matrix values

--- include/matrice.hpp
+++ include/matrice.hpp
@@ -3,20 +3,20 @@
 #include <string>
 
 using namespace std;
-template<typename T>
+template <typename T>
 class Vecteur;
 class Matrice
 {
-  public:
-	int n, m;	// Nombre de lignes, colonnes
+public:
+	int n, m;	  // Nombre de lignes, colonnes
 	double **val; // Tableau de valeurs
 
 	// Constructeurs
-	Matrice();								 // Constructeur par défaut
-	Matrice(int n_l, int n_c);				 // Constructeur avec dimension, initialisation à 0
+	Matrice();								  // Constructeur par défaut
+	Matrice(int n_l, int n_c);				  // Constructeur avec dimension, initialisation à 0
 	Matrice(int n_l, int n_c, double valeur); // Constructeur avec dimension + une valeur
-	Matrice(const Matrice &M);				 // Constructeur par copie
-	Matrice(int n, const string &id);		 // Contructeur matrice identité
+	Matrice(const Matrice &M);				  // Constructeur par copie
+	Matrice(int n, const string &id);		  // Contructeur matrice identité
 	// Destructeur
 	~Matrice();
 
@@ -39,6 +39,7 @@ class Matrice
 	Matrice &operator+=(const Matrice &M);
 	Matrice &operator-=(const Matrice &M);
 
+	void init();
 	double norm();
 	// Opérateurs d'affichage
 
@@ -48,30 +49,30 @@ class Matrice
 
 Matrice transpose(const Matrice &M);
 Vecteur<double> toVecteur(const Matrice &M);
-Matrice operator*(const Matrice& M,const double nb);
-Matrice operator*(const double nb,const Matrice& M);
+Matrice operator*(const Matrice &M, const double nb);
+Matrice operator*(const double nb, const Matrice &M);
 
 class Matrice_diag
 { // classe pour les matrices carrées diagonales
 public:
-  int n; //taille de la matrice carrée
-  double *val; //tableau des valeurs (diagonale uniquement)
+	int n;		 //taille de la matrice carrée
+	double *val; //tableau des valeurs (diagonale uniquement)
 
-  Matrice_diag();								 // Constructeur par défaut
+	Matrice_diag();						  // Constructeur par défaut
 	Matrice_diag(int n_l, double valeur); // Constructeur avec dimension + une valeur
 
-  // Destructeur
-  ~Matrice_diag();
+	// Destructeur
+	~Matrice_diag();
 
-  // clear
+	// clear
 	void clear();
 
-  friend ostream &operator<<(ostream &, const Matrice_diag &M);
-  Matrice_diag &operator=(const Matrice_diag &M);
-  Matrice_diag operator*(const double nb) const;
-  Vecteur<double> operator*(const Vecteur<double> &V) const;
+	friend ostream &operator<<(ostream &, const Matrice_diag &M);
+	Matrice_diag &operator=(const Matrice_diag &M);
+	Matrice_diag operator*(const double nb) const;
+	Vecteur<double> operator*(const Vecteur<double> &V) const;
 };
 
-Matrice_diag operator*(const double nb,const Matrice_diag & M);
+Matrice_diag operator*(const double nb, const Matrice_diag &M);
 
 #endif
