7a1e4e2e9c811b12965c1939e5811ec5dcde4dc3
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-03-11 09:24:59 +0100

    Fix header file path

--- src/vecteur.cpp
+++ src/vecteur.cpp
@@ -1,17 +1,18 @@
 #include <cstdlib>
 #include <cmath>
-#include "/home/othmane/shape/shape-from-shading/include/vecteur.hpp"
+#include "vecteur.hpp"
 
 //utilitaire de messages d'erreur
-void stop(const char * msg)
+void stop(const char *msg)
 {
-  cout<<"ERREUR classe vecteur, " << msg;
+  cout << "ERREUR classe vecteur, " << msg;
   exit(-1);
 }
 
-void test_dim(int d1, int d2, const char * org)
+void test_dim(int d1, int d2, const char *org)
 {
-  if(d1==d2)  return;
-  cout<<org<<" ("<<d1<<","<<d2<<") dimensions incompatibles";
+  if (d1 == d2)
+    return;
+  cout << org << " (" << d1 << "," << d2 << ") dimensions incompatibles";
   exit(-1);
 }
