9a2eded78a0da3ed134097cb5933853fec6c04f0
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-03-17 12:02:54 +0100

    Update python scripts

--- lib/img_to_csv.py
+++ lib/img_to_csv.py
@@ -1,13 +1,8 @@
-from PIL import Image # Importation du module 
-import csv # Importation du package csv
-import numpy as np # Importation de numpy 
-# Ouverture de l'image
-img = Image.open('../images/dragon.jpeg').convert('LA')
-img.save('greyscale.png')
+from PIL import Image  # Importation du module
+import csv  # Importation du package csv
+import numpy as np  # Importation de numpy
+import sys
 
-img = np.array(Image.open("greyscale.png"))
-
-# Définition de la matrice 
 
 def flatten(liste):
     flat_list = []
@@ -18,16 +13,28 @@ def flatten(liste):
         flat_list.append(ligne)
     return flat_list
 
-# Conversion de l'image en fichier csv    
 
 def img_to_csv(filename, array):
     list_array = array.tolist()
     list_array = flatten(list_array)
     print(len(list_array))
     print(len(list_array[0]))
-    with open(filename + '.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([len(list_array),len(list_array[0])])
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow([len(list_array), len(list_array[0])])
         writer.writerows(list_array)
-   
-img_to_csv("dragon", img)
+
+
+def main(input_filename, output_filename):
+    # Ouverture de l'image
+    img = Image.open(input_filename).convert('LA')
+    img_to_csv(output_filename, np.array(img))
+
+
+if __name__ == "__main__":
+    print(f"Arguments count: {len(sys.argv)}")
+    if(len(sys.argv) != 3):
+        print("Error: please provide input filename and output filename")
+    else:
+        main(sys.argv[1], sys.argv[2])
